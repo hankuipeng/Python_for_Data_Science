fc14b7ab4301d40a99958f8fa7790e52c88f79c4
hankuipeng <hankuipeng@gmail.com>
2018-10-15 14:51:52 +0100

    first commit 2018/10/15

---
+++ Week1.py
@@ -0,0 +1,53 @@
+# detect data types
+type(11)
+type(21.21)
+type(True)
+
+
+# type casting
+float(2)
+int('12')
+
+bool(1)
+int(True)
+
+ # division and integer division
+ 25/6
+ 25//6
+
+ # variables
+my_var = 1
+my_var
+
+# string
+name = "Python"
+name[0]
+name[-1]
+name[-3]
+
+name[::2] # return every other value in the string
+name[1::2]
+
+name[0:5:3] # get every third value within the first 4 positions
+
+len(name) # get the length of the string
+3*name
+
+name = name + " is the best"
+
+
+# transition between uppercase and lowercase
+name.upper()
+
+
+# replace part of the string
+name.replace("P", "C")
+name
+
+
+# string searching: outputs the first index of the sequence
+name.find("on")
+
+
+# string retrieving
+name[0:2]
